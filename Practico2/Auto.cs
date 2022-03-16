647eb883d9ff15026d638833141e76a3a97f36f8
Denis Arriola <denn.arr17@gmail.com>
2022-03-15 23:11:07 -0300

    Add project files.

---
+++ Practico2/Auto.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practico2
+{
+    class Auto
+    {
+        private string marca = "";
+        private string modelo = "";
+        private bool exoneraImpuestos;
+        private int anio = 0;
+        private string matricula = "";
+
+        public string Marca
+        {
+            get { return marca; }
+            set { marca = value; }
+        } 
+        public string Modelo
+        {
+            get { return modelo; }
+            set { modelo = value; }
+        }
+        public bool ExonerImpuestos
+        {
+            get { return exoneraImpuestos; }
+            set { exoneraImpuestos = value; }
+        }
+        public int Anio
+        {
+            get { return anio; }
+            set { anio = value; }
+        }
+        public string Matricula
+        {
+            get { return matricula; }
+            set { matricula = value; }
+        }
+
+        public static bool ValidarMatricula(string matricula)
+        {
+            return matricula.Length == 7;
+        }
+
+        public decimal CalcularPatente()
+        {
+            decimal patente = 0;
+            if (anio >= 2015)
+            {
+                patente = 17000;
+            }
+            else
+            {
+                if (exoneraImpuestos)
+                {
+                    patente = 10000;
+                }
+                else {
+                    patente = 12000;
+                }
+            }
+            return patente;
+        }
+    }
+}
---
+++ Practico2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Practico2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string marca = "Fiat";
+            string modelo = "uno";
+            bool exoneraImpuestos = true;
+            int anio = 2010;
+            string matricula = "jkt14215";
+
+
+            Auto obj = new Auto
+            {
+                Marca = marca,
+                Modelo = modelo,
+                ExonerImpuestos = exoneraImpuestos,
+                Anio = anio,
+                Matricula = matricula
+            };
+            Console.WriteLine(obj.Marca);
+            Console.WriteLine(obj.Modelo);
+            Console.WriteLine(obj.Anio);
+            Console.WriteLine(obj.Matricula);
+            Console.WriteLine(obj.Marca);
+
+            if (Auto.ValidarMatricula(matricula))
+            {
+                Console.WriteLine("la matricula es valida");
+            }
+            else
+            {
+                Console.WriteLine("la matricula NO es valida");
+            }
+
+            decimal patente = obj.CalcularPatente();
+            Console.WriteLine(patente);
+        }
+
+    }
+}
