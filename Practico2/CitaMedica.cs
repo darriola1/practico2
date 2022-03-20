6b2fbe3e723212f0add16ad3e18ce18dab3362f7
Denis Arriola <denn.arr17@gmail.com>
2022-03-19 21:59:56 -0300

    ADD EJ3

---
+++ Practico2/CitaMedica.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practico2
+{
+    class CitaMedica
+    {
+        // atributos
+        private int id;
+        private DateTime fecha;
+        private string cedula;
+        private string lugar;
+        private decimal precioBase;
+        private bool urgente;
+
+        // propiedades
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public DateTime Fecha
+        {
+            get { return fecha; }
+            set { fecha = value; }
+        }
+        public string Cedula
+        {
+            get { return cedula; }
+            set { cedula = value; }
+        }
+        public string Lugar
+        {
+            get { return lugar; }
+            set { lugar = value; }
+        }
+        public decimal PrecioBase
+        {
+            get { return precioBase; }
+            set { precioBase = value; }
+        }
+        public bool Urgente
+        {
+            get { return urgente; }
+            set { urgente = value; }
+        }
+
+        // metodos
+
+        public decimal CalcularCosto()
+        {
+            if (urgente)
+            {
+                return precioBase * 2;
+            }
+            return precioBase;
+        }
+    }
+}
--- Practico2/Program.cs
+++ Practico2/Program.cs
@@ -12,7 +12,7 @@ namespace Practico2
             string modelo = "uno";
             bool exoneraImpuestos = true;
             int anio = 2010;
-            string matricula = "jkt14215";
+            string matricula = "jkt1415";
 
 
             Auto obj = new Auto
@@ -24,13 +24,13 @@ namespace Practico2
                 Matricula = matricula
             };
 
-            Console.WriteLine("Ejercicio 1 - ");
+            Console.WriteLine("EJERCICIO 1");
             Console.WriteLine();
-            Console.WriteLine(obj.Marca);
-            Console.WriteLine(obj.Modelo);
-            Console.WriteLine(obj.Anio);
-            Console.WriteLine(obj.Matricula);
-            Console.WriteLine(obj.Marca);
+            Console.WriteLine($"MARCA: {obj.Marca}");
+            Console.WriteLine($"MODELO: {obj.Modelo}");
+            Console.WriteLine($"AÑO: {obj.Anio}");
+            Console.WriteLine($"MATRICULA: {obj.Matricula}");
+            
 
             if (Auto.ValidarMatricula(matricula))
             {
@@ -42,15 +42,15 @@ namespace Practico2
             }
 
             decimal patente = obj.CalcularPatente();
-            Console.WriteLine(patente);
+            Console.WriteLine($"PATENTE: {patente}");
             Console.WriteLine();
-            Console.WriteLine("Fin ejercicio 1"); 
+            Console.WriteLine("FIN EJERCICIO 1");
             Console.WriteLine();
 
             //EJERCICIO2
             string nombre = "Denis";
             string apellido = "Arriola";
-            DateTime fechaNacimiento = new DateTime(1991,06,22);
+            DateTime fechaNacimiento = new DateTime(1991, 06, 22);
             decimal valorHora = 100;
             int antiguedad = 9;
             int horasTrabajadas = 200;
@@ -65,11 +65,40 @@ namespace Practico2
                 HorasTrabajadas = horasTrabajadas,
 
             };
-            Console.WriteLine("Ejercicio 2");
+            Console.WriteLine("EJERCICIO 2");
             Console.WriteLine();
             Console.WriteLine(unEmpleado);
             Console.WriteLine();
-            Console.WriteLine("fin ejercicio 2");
+            Console.WriteLine("FIN EJERCICIO 2");
+            Console.WriteLine();
+
+            // EJERCICIO 3
+            Console.WriteLine("EJERCICIO 3");
+            int id = 1;
+            DateTime fecha = new DateTime(2021, 03, 19);
+            string cedula = "44042498";
+            string lugar = "Montevideo";
+            decimal precioBase = 10;
+            bool urgente = true;
+
+            CitaMedica nuevaCita = new CitaMedica
+            {
+                Id = id,
+                Fecha = fecha,
+                Cedula = cedula,
+                Lugar = lugar,
+                PrecioBase = precioBase,
+                Urgente = urgente,
+            };
+
+            Console.WriteLine();
+            Console.WriteLine($"Id de la cita {nuevaCita.Id}");
+            Console.WriteLine($"La fecha de la cita es: {nuevaCita.Fecha}");
+            Console.WriteLine($"Documento: {nuevaCita.Cedula}");
+            Console.WriteLine($"En la sucursal de: {nuevaCita.Lugar}");
+            Console.WriteLine($"El usuario debe abonar: {nuevaCita.CalcularCosto()} pesos");
+            Console.WriteLine();
+            Console.WriteLine("FIN EJERCICIO 3");
         }
 
     }
